436ab492771b5d4872fa47160582b82dd7c41b30
Enes Karademir <ekarademirr@gmail.com>
2019-05-14 14:51:04 +0300

    Add files via upload
    Inital Commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+    },
+    mode: 'development',
+	module: {
+		rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader' , 'less-loader']
+            },
+            {
+                test: /\.pug$/,
+                use: ['html-loader?attrs=false', 'pug-html-loader']
+            },
+            { test: /\.handlebars$/, loader: "handlebars-loader" }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'home.html',
+            template: 'template/home.handlebars',
+            inject: false,
+            minify:false
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'about.html',
+            template: 'template/about.pug',
+            inject: false,
+            minify:false
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'contact.html',
+            template: 'template/contact.handlebars',
+            inject: false,
+            minify:false
+        })
+    ],
+    watch: true
+};
